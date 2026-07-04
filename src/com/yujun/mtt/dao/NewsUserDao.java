3c4fe4b5cb5579428ace87b0a39960637ba7b7d6
code-YuJun <2594131385@qq.com>
2026-07-04 14:31:26 +0800

    feat: 注册登录功能开发

--- src/com/yujun/mtt/dao/NewsUserDao.java
+++ src/com/yujun/mtt/dao/NewsUserDao.java
@@ -1,8 +1,25 @@
 package com.yujun.mtt.dao;
 
-/**
- * ClassName: NewsUserDao
- * Package: com.yujun.mtt.dao
- */
+import com.yujun.mtt.pojo.NewsUser;
+
 public interface NewsUserDao {
+    /**
+     * 根据用户名查询用户信息
+     * @param username 要查询的用户名
+     * @return 找到返回NewsUser对象,找不到返回null
+     */
+    NewsUser findByUserName(String username);
+
+    /**
+     * 根据用户id连接数据库查询用户信息
+     * @param uid 要查询的用户id
+     * @return 找到返回NewsUser对象,找不到返回null
+     */
+    NewsUser findByUid(Integer uid);
+    /**
+     * 将用户信息存入数据库
+     * @param newsUser
+     * @return
+     */
+    int insertNewsUser(NewsUser newsUser);
 }
--- src/com/yujun/mtt/service/NewsUserService.java
+++ src/com/yujun/mtt/service/NewsUserService.java
@@ -1,8 +1,26 @@
 package com.yujun.mtt.service;
 
-/**
- * ClassName: NewsUserService
- * Package: com.yujun.mtt.service
- */
+import com.yujun.mtt.pojo.NewsUser;
+
 public interface NewsUserService {
+    /**
+     * 根据用户名,获得查询用户的方法
+     * @param username 要查询的用户名
+     * @return 如果找到返回NewsUser对象,找不到返回null
+     */
+    NewsUser findByUserName(String username);
+
+    /**
+     * 根据用户id查询用户信息
+     * @param uid 要查询的用户id
+     * @return 找到返回NewsUser对象,找不到返回null
+     */
+    NewsUser findByUid(Integer uid);
+
+    /**
+     * 注册用户信息,注册成功返回大于0的整数,失败返回0
+     * @param newsUser
+     * @return
+     */
+    int registUser(NewsUser newsUser);
 }
